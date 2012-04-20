99866f63468f484bcbdf4b8098e9368f500f9d69
Jonathan Linowes <linojon@gmail.com>
2012-04-20 15:20:40 -0400

    change category and doctype to select lists

--- vendor/extensions/documents/app/models/refinery/documents/document.rb
+++ vendor/extensions/documents/app/models/refinery/documents/document.rb
@@ -1,11 +1,15 @@
 module Refinery
   module Documents
     class Document < Refinery::Core::BaseModel
-      self.table_name = 'refinery_documents'      
+      self.table_name = 'refinery_documents'     
+      CATEGORIES = ["Early History", "Major Contributors", "National Hay Fever Association", "BHC Sisterhood", "Other"]
+      DOCTYPES = ["Newspaper", "Photo", "Minutes", "Other"]
     
       acts_as_indexed :fields => [:title, :category, :subject, :doctype, :description, :source]
 
       validates :title, :presence => true, :uniqueness => true
+      validates :category, :inclusion => {:in => CATEGORIES}
+      validates :doctype, :inclusion => {:in => DOCTYPES}
           
       belongs_to :image, :class_name => '::Refinery::Image'
         
